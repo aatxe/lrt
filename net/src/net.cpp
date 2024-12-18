05eb8166c0126e785d1b178f9d54e060a97a531d
Vyacheslav Egorov <vegorov@roblox.com>
2024-12-18 11:28:02 -0800

    Switch async network requests to libuv work queue

--- net/src/net.cpp
+++ net/src/net.cpp
@@ -4,6 +4,8 @@
 
 #include "curl/curl.h"
 
+#include "uv.h"
+
 #include "lua.h"
 #include "lualib.h"
 
@@ -32,6 +34,8 @@ static std::pair<std::string, std::vector<char>> requestData(const std::string& 
     if (!curl)
         return { "failed to initialize", {} };
 
+    printf("Requested %s at %d\n", url.c_str(), clock());
+
     std::vector<char> data;
 
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
@@ -43,6 +47,8 @@ static std::pair<std::string, std::vector<char>> requestData(const std::string& 
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 
     CURLcode res = curl_easy_perform(curl);
+
+    printf("Finished %s at %d\n", url.c_str(), clock());
 
     if (res != CURLE_OK)
         return { curl_easy_strerror(res), {} };
@@ -71,8 +77,41 @@ static int getAsync(lua_State* L)
     auto ref = getRefForThread(L);
     Runtime* runtime = getRuntime(L);
 
+    auto loop = uv_default_loop();
+
+    struct WorkData
+    {
+        decltype(ref) ref;
+        decltype(runtime) runtime;
+        decltype(url) url;
+    };
+
+    uv_work_t *work = new uv_work_t();
+    work->data = new WorkData{ ref, runtime, url };
+
+    uv_queue_work(loop, work, [](uv_work_t* req) {
+        WorkData& workData = *(WorkData*)req->data;
+
+        auto [error, data] = requestData(workData.url);
+
+        if (!error.empty())
+        {
+            workData.runtime->scheduleLuauError(workData.ref, "network request failed: " + error);
+        }
+        else
+        {
+            workData.runtime->scheduleLuauResume(workData.ref, [data = std::move(data)](lua_State* L) {
+                lua_pushlstring(L, data.data(), data.size());
+                return 1;
+                });
+        }
+    }, [](uv_work_t* req, int status) {
+        delete (WorkData*)req->data;
+        delete req;
+    });
+
     // TODO: switch to libuv, add cancellations
-    std::thread thread = std::thread([=] {
+    /*std::thread thread = std::thread([=] {
         auto [error, data] = requestData(url);
 
         if (!error.empty())
@@ -88,7 +127,7 @@ static int getAsync(lua_State* L)
         }
     });
 
-    thread.detach();
+    thread.detach();*/
 
     return lua_yield(L, 0);
 }

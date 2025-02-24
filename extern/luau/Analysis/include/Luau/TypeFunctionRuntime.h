e2088a063e54f0430f77ad3b119600ad6af36d19
Ariel Weiss <aweiss@hey.com>
2025-02-23 19:18:38 -0800

    git subrepo commit (merge) extern/luau
    subrepo:
      subdir:   "extern/luau"
      merged:   "45cd22df"
    upstream:
      origin:   "git@github.com:luau-lang/luau.git"
      branch:   "master"
      commit:   "c1e2f650"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/Homebrew/brew"
      commit:   "2e57f8f794"

--- extern/luau/Analysis/include/Luau/TypeFunctionRuntime.h
+++ extern/luau/Analysis/include/Luau/TypeFunctionRuntime.h
@@ -3,6 +3,7 @@
 
 #include "Luau/Common.h"
 #include "Luau/Variant.h"
+#include "Luau/TypeFwd.h"
 
 #include <optional>
 #include <string>
@@ -216,6 +217,16 @@ struct TypeFunctionClassType
     std::optional<TypeFunctionTypeId> metatable; // metaclass?
 
     std::optional<TypeFunctionTypeId> parent;
+
+    TypeId classTy;
+
+    std::string name_DEPRECATED;
+};
+
+struct TypeFunctionGenericType
+{
+    bool isNamed = false;
+    bool isPack = false;
 
     std::string name;
 };
